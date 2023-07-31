970b7f5e681573b922a66aa3f5b29a8aa2ff04c8
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 22:39:51 +0530

    Update ch3_Q4.c

--- ch3_Q4.c
+++ ch3_Q4.c
@@ -1,1 +1,29 @@
+/*
+Write a program to find whether a year entered by the user is a leap year or not. Take year as an input from the user.
+*/
 
+#include <stdio.h>
+
+int main() 
+{
+  int year;
+  printf("Provide year : ");
+  scanf("%d", &year);
+  if (year % 4 == 0){
+    if (year % 100 == 0){
+      if(year % 400 == 0){
+        printf("Leap Year.\n");
+      }
+      else {
+        printf("Not a Leap Year.\n");
+      }
+    }
+    else {
+      printf("Leap Year.\n");
+    }
+  }
+  else {
+    printf("Not a Leap Year.\n");
+  }
+  return 0;
+}
