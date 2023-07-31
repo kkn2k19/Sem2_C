a66a3cf0ab8d2bed93bae101c4050a1db1f97816
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 23:03:34 +0530

    Create ch3_Q6.c

---
+++ ch3_Q6.c
@@ -0,0 +1,25 @@
+/*
+Write a program to find greatest of four numbers entered by the user.
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+  int a, b, c, d;
+  printf("Enter four numbers : ");
+  scanf("%d %d %d %d", &a, &b, &c, &d);
+  if (a > b && a > c && a > d) {
+    printf("%d is greatest.\n", a);
+  }
+  else if (b > a && b > c && b > d) {
+    printf("%d is greatest.\n", b);
+  }
+  else if (c > a && c > b && c > d) {
+    printf("%d is greatest.\n", c);
+  }
+  else {
+    printf("%d is greatest.\n", d);
+  }
+  return 0;
+}
