28a38fbf9ad8257d272c3b27662a74a567886a1b
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 16:35:43 +0530

    Create ch_Q1_a.c

---
+++ ch_Q1_a.c
@@ -0,0 +1,15 @@
+/*
+Write a C program to calculate area of a rectangle 
+  (a) Using hard coded inputs 
+  (b) Using Inputs supplied by the user.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int l = 40;
+  int b = 30;
+  float area = l*b;
+  printf("Area of Rectangle : %f", area);
+}
