6a24dbd6a35653e32a7ac2213618f448b488adee
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 16:26:16 +0530

    Create first_program.c

---
+++ first_program.c
@@ -0,0 +1,10 @@
+//Writing my first C program.
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello! there, It's me KKN.\n");
+  printf("Started Learning C programming language Today.");
+  return 0;
+}
