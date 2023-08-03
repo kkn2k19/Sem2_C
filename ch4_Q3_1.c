d062ac6fa0bff0518096111fb0a8e6939c3cd932
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-03 05:52:27 +0530

    Create ch4_Q3_1.c

---
+++ ch4_Q3_1.c
@@ -0,0 +1,11 @@
+//A do while is executed at least once
+
+#include <stdio.h>
+
+int main() 
+{
+  do {
+    printf("Loop ran One time.\n");
+  } while (0);                   // 0 means false condition.
+  return 0;
+}
