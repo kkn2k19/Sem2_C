03f0ba4edae5adf21e009d14d55b4533075a650e
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-04 11:32:07 +0530

    Create ch7_quick_quiz2.c

---
+++ ch7_quick_quiz2.c
@@ -0,0 +1,22 @@
+/*
+Following operations can be performed on a pointers :- 
+1. Addition of a number to a pointer.
+2. Subtraction of a number from a pointer.
+3. Subtraction of one pointer from another.
+4. Comparison of two pointer variables.
+Try these operations on another variable by creating pointers in a separate program.
+Demonstrate all the four operations.
+*/
+
+
+
+NOT COMPLETE YET!!!!
+
+
+
+#include <stdio.h>
+
+int main () 
+{
+  
+}
