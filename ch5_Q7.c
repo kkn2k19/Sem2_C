7ead37041af3ca0ab291da92c0587989be027ddd
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-04 09:52:56 +0530

    Create ch5_Q7.c

---
+++ ch5_Q7.c
@@ -0,0 +1,29 @@
+/*
+Write a program  using functions to print the following pattern (first n lines)
+
+*
+***
+*****
+
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  int n;
+  printf("Enter n : ");
+  scanf("%d", &n);
+  pattern(n);
+  return 0;
+}
+
+void pattern (int n) 
+{
+  int if (n == 1) {
+    printf("*\n");
+  }
+  else {
+    
+  }
+}
