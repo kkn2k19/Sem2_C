6ad611d4bc18b95ae22533480101ea77363accfc
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-04 09:27:59 +0530

    Update ch5_Q3.c

--- ch5_Q3.c
+++ ch5_Q3.c
@@ -1,2 +1,15 @@
 //Write a function to calculate force of attraction on a body of mass m exerted by Earth. (g = 9.8m/s^2)
 
+#include <stdio.h>
+
+int main () 
+{
+  int m, G, F, M, R;
+  printf("Enter mass of body : ");
+  scanf("%d", &m);
+  G = 6.67 * pow(10, -11);
+  M = 5.98 * pow(10, 24);
+  R = 6.37 * pow(10, 6);
+  F = GMm/(R*R);
+  printf("Force of Attraction on body by Earth : %d.\n", F);
+}
