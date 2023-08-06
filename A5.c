ba746bf63318d6a52ec699dab6a8d2c26ac68390
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 10:47:18 +0530

    Create A5.c

---
+++ A5.c
@@ -0,0 +1,31 @@
+/*
+Write a C program to print a block F using hash (#), where the F has a height of six
+characters and width of five and four characters. And also, to print a big 'C'.
+Expected Output:
+       ######
+       #
+       #
+       #####
+       #
+       #
+       #
+ 
+          ######
+        ##      ##
+       #
+       #
+       #
+       #
+       #
+        ##      ##
+          ###### 
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  printf("######\n#\n#\n#####\n#\n#\n#\n");
+  printf("\n   ######\n ##      ##\n#\n#\n#\n#\n#\n ##      ##\n   ######\n");
+  return 0;
+}
