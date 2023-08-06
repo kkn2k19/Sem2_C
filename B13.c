5c144911f1b5914f044a534a20604ad96718fdfb
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 15:56:50 +0530

    Create B13.c

---
+++ B13.c
@@ -0,0 +1,26 @@
+/*
+Write a program to input three numbers (positive or negative). If they are unequal then
+display the greatest number otherwise, display they are equal. The program also displays
+whether the numbers entered by the user are 'All positive', 'All negative' or 'Mixed
+numbers'.
+Sample Input: 56, -15, 12
+Sample Output:
+The greatest number is 56
+Entered numbers are mixed numbers. 
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  int a, b, c;
+  printf("Provide Three Numbers negative or positive : ");
+  scanf("%d%d%d", &a, &b, &c);
+  if (a > b && a > c){
+    if (a > 0 )
+
+
+NOT COMPLETE YET !!!!
+      
+  }
+}
