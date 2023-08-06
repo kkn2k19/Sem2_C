3c9b84dc73219d90d6f4669fee54964af5342abc
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 12:20:16 +0530

    Create B2.c

---
+++ B2.c
@@ -0,0 +1,16 @@
+/*
+WAP to take the Sale amount as input and provide the commission according to the
+following table
+Sale Amount                 Commission
+Less than 500                 Rs. 35
+From 500 to 2000         10% of Sale Amount
+From 2001 to 5000        15% of Sale Amount
+More than 5000           20% of Sale Amount
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  NOT COMPLETE YET!!!!
+}
