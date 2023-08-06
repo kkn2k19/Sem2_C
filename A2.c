9fd3a6327a170bde02ae3c2d6eefd23df665e8df
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 10:33:50 +0530

    Create A2.c

---
+++ A2.c
@@ -0,0 +1,17 @@
+/*
+WAP in C to print “INDIAN” in the following way:
+I
+IN
+IND
+INDI
+INDIA
+INDIAN
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  printf("I\nIN\nIND\nINDI\nINDIA\nINDIAN");
+  return 0;
+}
