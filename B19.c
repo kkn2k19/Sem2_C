fc9cf1bcd6a15ddcdb187338ab398d965b5a7eb1
scr <141555910+scr7304@users.noreply.github.com>
2023-08-07 00:00:39 +0530

    Create B19.c

---
+++ B19.c
@@ -0,0 +1,24 @@
+/*
+'Kumar Electronics' has announced the following seasonal discounts on purchase of certain
+items.
+Purchase Amount                       Discount on Laptop                     Discount on Desktop PC
+Up to ₹ 25000                                 0.0%                                   5.0%
+₹ 25,001 to ₹ 50,000                           5%                                    7.5%
+₹ 50,001 to ₹ 1,00,000                        7.5%                                  10.0%
+More than ₹ 1,00,000                         10.0%                                  15.0%
+Write a program to input name, amount of purchase and the type of purchase (`L' for
+Laptop and 'D' for Desktop) by a customer. Compute and print the net amount to be paid
+by a customer along with his name.
+(Net amount = Amount of purchase - discount) 
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+
+
+NOT COMPLETE YET!!!!!!
+
+
+}
