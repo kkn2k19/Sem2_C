f5affdfee99c07c1d560b1606dd4448b1efce189
cde <141555910+scr7304@users.noreply.github.com>
2023-08-06 22:54:51 +0530

    Create B14.c

---
+++ B14.c
@@ -0,0 +1,33 @@
+/*
+An air-conditioned bus charges fare from the passengers based on the distance travelled as
+per the tariff given below:
+Distance Travelled                         Fare
+Up to 10 km                         Fixed charge ₹80
+11 km to 20 km                            ₹6/km
+21 km to 30 km                            ₹5/km
+31 km and above                           ₹4/km 
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  int d;
+  int f;
+  printf("Provide Distance Travelled : ");
+  scanf("%f", &d);
+  if (d <= 10) {
+    f = 80;
+  }
+  else if (d >= 11 && d <= 20) {
+    f = 6*d;
+  }
+  else if (d >= 21 && d <= 30) {
+    f = 5*d;
+  }
+  else {
+    f = 4*d;
+  }
+  printf("Fare Amount : %d", f);
+  return 0;
+}
