d4e036fcca519eedb4e690159289d87b41ac2432
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 14:58:42 +0530

    Create B7.c

---
+++ B7.c
@@ -0,0 +1,23 @@
+/*
+Write a C program to remove any negative sign in front of a number.
+Input a value (negative):
+ Expected Output:
+Original value = -253
+Absolute value = 253
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  int a;
+  printf("Provide a number negative or positive : ");
+  scanf("%d", &a);
+  if (x < 0) {
+    printf("Absolute value = %d\n", -1*x);
+  }
+  else {
+    printf("Absolute value = %d\n", x);
+  }
+  return 0;
+}
