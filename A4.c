a86e905d73b2cfef3dbc0bfb6b9598c350d23daf
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 10:41:26 +0530

    Create A4.c

---
+++ A4.c
@@ -0,0 +1,16 @@
+/*
+Write a C program to print your name, date of birth and mobile number.
+Expected Output:
+Name : Dennis MacAlistair Ritchie
+DOB : September 9, 1941
+Mobile: 99-9999999999
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  printf("Name : Karan Kumar Nonia\n");
+  printf("DOB : September 6, 2004\n");
+  printf("+91-9722093043\n");
+}
