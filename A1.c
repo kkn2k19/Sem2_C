072066f303ba32c4a70a168651a0001a15c8d2cb
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 10:32:12 +0530

    Create A1.c

---
+++ A1.c
@@ -0,0 +1,12 @@
+/*
+WAP in c to print the following information in the following way:
+Asansol Engineering College
+IT Dept. 
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  printf("Asansol Engineering College\nIT Dept.");
+}
