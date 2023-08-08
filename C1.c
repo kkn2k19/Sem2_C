a45518dfb139ddce4ef3995635d2b7e0b2b243a5
scr <141555910+scr7304@users.noreply.github.com>
2023-08-08 07:50:43 +0530

    Create C1.c

---
+++ C1.c
@@ -0,0 +1,16 @@
+/*
+Write a program in C to take 10 numbers as input and display how many of them are Odd 
+number and Even number. 
+Test Data : 
+10 11 75 89 56 44 22 77 55 65
+Expected Output : 
+Total number of “ODD” numbers: 6 
+Total number of “EVEN” numbers:4 
+Test Data : 
+10 20 30 41 50 51 60 6 8 10 
+Expected Output : 
+Total number of “ODD” numbers: 2 
+Total number of “EVEN” numbers:8 
+*/
+
+NOT COMPLETE YET!!!!
