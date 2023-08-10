ddca9d21390850893e4b577f0e8726bc86dc3ea0
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-11 00:08:48 +0530

    Create ch8_quick_quiz1.c

---
+++ ch8_quick_quiz1.c
@@ -0,0 +1,15 @@
+/*
+Create a String using "" and prints its content a loop.
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  char name[] = "KARAN";
+  int i;
+  for (i = 0; i < strlen(name); i++) {
+    printf("%c", name[i]);
+  }
+  return 0;
+}
